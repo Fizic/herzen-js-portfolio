fcbaa880cd3c1c7a49a7c8a4b2880fd0d33eb27c
Fizic <kirya.firsov.04@gmail.com>
2023-12-28 17:50:01 +0500

    feat: lab_3 task_1

---
+++ static/js/lab_3/task_1.js
@@ -0,0 +1,31 @@
+window.onload = () => {
+    class WeatherDisplay {
+        constructor(cityTitle) {
+            this.fetchWeather(cityTitle).then(json => {
+                this.parseWeather(json)
+                this.render(`Temperature: ${this.temp}C, Humidity: ${this.humidity}%`)
+            })
+        }
+
+        async fetchWeather(cityTitle) {
+            const res = await fetch(`https://wttr.in/${cityTitle}?format=j2`).catch(err => {
+                this.render(err.message)
+            })
+            return await res.json()
+        }
+
+        parseWeather(json) {
+            this.temp = json.current_condition[0].temp_C
+            this.humidity = json.current_condition[0].humidity
+        }
+
+        render(content) {
+            const weatherContainer = document.getElementById('weather-container')
+            weatherContainer.innerText = content
+        }
+    }
+    document.getElementById('search-button').addEventListener('click', () => {
+        const cityTitle = document.getElementById('city-input').value
+        new WeatherDisplay(cityTitle)
+    })
+}
